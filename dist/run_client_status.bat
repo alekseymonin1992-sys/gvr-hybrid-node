97c9b9777cba550363c405e6a6191ff78adb3376
alekseymonin1992-sys <alekseymonin1992@gmail.com>
2026-03-23 01:35:28 +0700

    Stop tracking dist folder (build artifacts)

--- dist/run_client_status.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d %~dp0
-
-echo GVR Client: status
-echo.
-
-client.exe status --rpc 127.0.0.1:8080
-
-pause
--- dist/run_wallet_new_alice.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d %~dp0
-
-echo GVR Wallet: create wallet 'alice'
-echo.
-
-wallet.exe new --name alice
-
-pause
